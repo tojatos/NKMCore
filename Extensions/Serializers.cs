2f7701771983bffde7d805b949b04c3785a114a8
tojatos <tojatos@gmail.com>
2019-09-23 23:19:06 +0200

    Serializers - added PickType, removed Character
    Seems like I will serialize GamePreparerDependencies instead, as it
    makes more sense.

--- Extensions/Serializers.cs
+++ Extensions/Serializers.cs
@@ -5,13 +5,9 @@ namespace NKMCore.Extensions
 {
     public static class Serializers
     {
-//        public static List<GamePlayer> DeserializeGamePlayers(this string str) => new List<GamePlayer>();
-//        public static string Serialize(this List<GamePlayer> gamePlayers) => "";
-//        public static GamePlayer DeserializeGamePlayer(this string str) => new GamePlayer();
-//        public static string Serialize(this GamePlayer gamePlayer) => "";
         public static GameType DeserializeGameType(this string str) => str.ToEnum<GameType>();
         public static string Serialize(this GameType gameType) => gameType.ToString();
-        public static Character DeserializeCharacter(this string str) => new Character(new Character.Properties{Name = str});
-        public static string Serialize(this Character character) => character.Name;
+        public static PickType DeserializePickType(this string str) => str.ToEnum<PickType>();
+        public static string Serialize(this PickType pickType) => pickType.ToString();
     }
 }
